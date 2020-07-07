9133fd13bc2c34e94d1445011aa13e91ed85f82e
lukelliotfrost <luke@radio.co>
2020-07-07 19:00:48 +0100

    Set up one to many relationship with BlogPost and Comments.

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -6,7 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class BlogPost extends Model
 {
-    protected $table = 'blogposts';
+    //protected $table = 'blogposts';
 
     protected $fillable = ['title', 'content'];
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
